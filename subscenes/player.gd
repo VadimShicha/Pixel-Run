e234b798ee360833b7808b93e89560c13c176d56
VadimShicha <75353180+VadimShicha@users.noreply.github.com>
2024-04-27 00:12:01 -0700

    Gravity flipping

--- subscenes/player.gd
+++ subscenes/player.gd
@@ -1,24 +1,37 @@
 extends CharacterBody2D
 
-
 const SPEED = 400
 const JUMP_VELOCITY = -770
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+var gravity_direction = 1
+
+func _input(event):
+	if event.is_action_pressed("left_mouse_click"):
+		gravity_direction *= -1
+		
 
 func _physics_process(delta):
-	# Add the gravity.
+	print(velocity)
+	
+	# Add the gravity
 	if not is_on_floor():
-		velocity.y += gravity * 1.82 * delta
+		if gravity_direction == 1:
+			velocity.y += gravity * 1.72 * delta
+	if not is_on_ceiling():
+		if gravity_direction == -1:
+			velocity.y -= gravity * 1.72 * delta
+		
+	# Handle jump
+	if Input.is_action_just_pressed("jump"):
+		if gravity_direction == 1 and is_on_floor():
+			velocity.y = JUMP_VELOCITY
+		elif gravity_direction == -1 and is_on_ceiling():
+			velocity.y = -JUMP_VELOCITY
 
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Get the input direction and handle the movement/deceleration
 	var direction = Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
