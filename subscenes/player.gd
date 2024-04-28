c613d531d51b7461aec8bb11fb423825f2e8f67b
VadimShicha <75353180+VadimShicha@users.noreply.github.com>
2024-04-27 21:45:56 -0700

    Mouse movement

--- subscenes/player.gd
+++ subscenes/player.gd
@@ -1,46 +1,27 @@
-extends CharacterBody2D
-
-@onready var sprite_2d = $Sprite2D
+extends RigidBody2D
 
 const SPEED = 400
 const JUMP_VELOCITY = -770
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
-var gravity_direction = 1
-
-func _input(event):
-	if event.is_action_pressed("left_mouse_click"):
-		if gravity_direction == 1:
-			gravity_direction = -1
-			sprite_2d.flip_v = true
-		else:
-			gravity_direction = 1
-			sprite_2d.flip_v = false
-		
+var move_keys_just_pressed = false # indicates whether the left or right keys were pressed last frame
 
 func _physics_process(delta):
-	# Add the gravity
-	if not is_on_floor():
-		if gravity_direction == 1:
-			velocity.y += gravity * 2 * delta
-	if not is_on_ceiling():
-		if gravity_direction == -1:
-			velocity.y -= gravity * 2 * delta
-		
-	# Handle jump
-	if Input.is_action_just_pressed("jump"):
-		if gravity_direction == 1 and is_on_floor():
-			velocity.y = JUMP_VELOCITY
-		elif gravity_direction == -1 and is_on_ceiling():
-			velocity.y = -JUMP_VELOCITY
+	
+	# handle
+	if Input.is_action_just_pressed("left_mouse_click"):
+		var mouse_position = get_global_mouse_position()
+		var direction = (mouse_position - position).normalized()
+		apply_impulse(direction * 1100)
 
-	# Get the input direction and handle the movement/deceleration
+	# handle jumping
+	if Input.is_action_just_pressed("jump"):
+		linear_velocity.y = JUMP_VELOCITY
+
+	# handle movement and stop the player once key is released
 	var direction = Input.get_axis("left", "right")
 	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
+		linear_velocity.x = direction * SPEED
+		move_keys_just_pressed = true
+	elif move_keys_just_pressed and linear_velocity.x != 0:
+		linear_velocity.x = move_toward(linear_velocity.x, 0, SPEED)
+		move_keys_just_pressed = false
