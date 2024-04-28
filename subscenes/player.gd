7f08b7f87ef861243ab4786fce171c33559c7a1d
VadimShicha <75353180+VadimShicha@users.noreply.github.com>
2024-04-28 16:23:12 -0700

    Frog texture

--- subscenes/player.gd
+++ subscenes/player.gd
@@ -1,25 +1,52 @@
 extends RigidBody2D
 
+@onready var frog_arrow = $FrogArrow
+@onready var sprite_2d = $Sprite2D
+
+
+const FROG_ARROW_CENTER_OFFSET = 70
 const SPEED = 400
 const JUMP_VELOCITY = -770
 
 var move_keys_just_pressed = false # indicates whether the left or right keys were pressed last frame
-
-func _physics_process(delta):
 	
-	# handle
-	if Input.is_action_just_pressed("left_mouse_click"):
+func _process(delta):
+	var mouse_position_relative = get_global_mouse_position() - position
+	var mouse_direction = mouse_position_relative.normalized()
+	
+	if mouse_position_relative.x > 0:
+		frog_arrow.rotation_degrees = rad_to_deg(atan(mouse_direction.y / mouse_direction.x)) + 90
+	elif mouse_position_relative.x < 0:
+		frog_arrow.rotation_degrees = rad_to_deg(atan(mouse_direction.y / mouse_direction.x)) + 270
+		
+	frog_arrow.position = mouse_direction * FROG_ARROW_CENTER_OFFSET
+	
+	
+func _physics_process(delta):
+	var is_grounded = snapped(linear_velocity.y, 0.05) == 0
+	
+	if is_grounded:
+		sprite_2d.animation = "default"
+		frog_arrow.show()
+	else:
+		sprite_2d.animation = "fall"
+		frog_arrow.hide()
+		
+	# handle hopping
+	if is_grounded and Input.is_action_just_pressed("left_mouse_click"):
 		var mouse_position = get_global_mouse_position()
 		var direction = (mouse_position - position).normalized()
+		sprite_2d.flip_h = (direction.x > 0)
 		apply_impulse(direction * 1100)
 
 	# handle jumping
-	if Input.is_action_just_pressed("jump"):
+	if is_grounded and Input.is_action_just_pressed("jump"):
 		linear_velocity.y = JUMP_VELOCITY
 
 	# handle movement and stop the player once key is released
 	var direction = Input.get_axis("left", "right")
 	if direction:
+		sprite_2d.flip_h = (direction == 1)
 		linear_velocity.x = direction * SPEED
 		move_keys_just_pressed = true
 	elif move_keys_just_pressed and linear_velocity.x != 0:
